eef76ce7cc681ddff3f735df9d9ab216c9b2edc7
Rajan-sust <imrajon0@gmail.com>
2024-05-23 15:37:37 +0600

    font color added

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from util import get_content_from_pdf, get_formatted_resume_content
 import uuid
 import shutil
 import os
-from util import get_content_from_pdf, get_formatted_resume_content
 import json
+import datetime
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "https://example.com",
+# ]
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specified origins
+    allow_credentials=True,  # Allows cookies to be included
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.get("/health")
@@ -49,10 +68,16 @@ def create_upload_file(file: UploadFile):
             'message': 'File saving issue'
         }
 
+    modification_time = os.path.getmtime(saved_filepath)
+
+    # Convert the timestamp to a readable date and time format
+    file_information["modified_date"] = datetime.datetime.fromtimestamp(modification_time).strftime('%Y-%m-%d')
+
     pdf_content, presentation = get_content_from_pdf(saved_filepath)
     os.remove(saved_filepath)
 
-    pdf_parsed_info = json.loads(get_formatted_resume_content(pdf_content))
+    # pdf_parsed_info = json.loads(get_formatted_resume_content(pdf_content))
+    pdf_parsed_info = dict()
     pdf_parsed_info.update({'file_information': file_information})
     pdf_parsed_info.update({'presentation': presentation})
 
--- util.py
+++ util.py
@@ -4,23 +4,44 @@ import json
 
 client = OpenAI()
 
+
+def get_decimal_to_rgb_color(color):
+    # Convert to hexadecimal and remove the '0x' prefix
+    color_hex = f"{color:06x}"
+    # print(color_hex)
+    # Extract the red, green, and blue components
+    red = int(color_hex[0:2], 16)
+    green = int(color_hex[2:4], 16)
+    blue = int(color_hex[4:6], 16)
+
+    # Combine into an RGB tuple
+    return (red, green, blue)
+
+
 def get_content_from_pdf(file_path):
     doc = pymupdf.open(file_path)
     text = ''
     for page in doc:
         text += page.get_text()
     fonts = set()
+    font_color = set()
 
     for page in doc:
         for block in page.get_text("dict")["blocks"]:
+            # print(block)
             for line in block["lines"]:
                 for span in line["spans"]:
                     fonts.add(span['font'])
+                    # print(span['color'])
+                    font_color.add(get_decimal_to_rgb_color(span['color']))
+
     # print(fonts)
+    font_color_list = [[r, g, b] for r, g, b in font_color]
     presentation = {
         'page_count': doc.page_count,
         'word_count': len(text.split()),
-        'fonts': list(fonts)
+        'fonts': list(fonts),
+        'font_color': font_color_list
     }
     # print(presentation)
     return text, presentation
@@ -88,3 +109,4 @@ def get_formatted_resume_content(content):
 
 if __name__ == '__main__':
     get_content_from_pdf('uploads/2b18ceb6-8aef-4289-b2bc-db2f94375733.pdf')
+    # get_decimal_to_rgb_color(0)
